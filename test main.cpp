9f960c4c4957be9beea6d16e343acf43acc8bc3c
damianww <ltsdltsd@163.com>
2014-06-21 13:02:39 +0800

    Create test main.cpp

---
+++ test main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <stdlib>
+
+using namespace std;
+
+int main()
+{
+  cout<<"this is my first project in github, i will continue learning and working in it!"<<endl;
+  return 0;
+}
